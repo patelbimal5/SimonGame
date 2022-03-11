9b62a624cf8e49b1707ae41bf7037bf318c56038
patelbimal5 <68713222+patelbimal5@users.noreply.github.com>
2022-03-11 14:22:12 -0500

    adding more functioning codes.
    adding to choose random colors and playing sound related to chosen color.

--- game.js
+++ game.js
@@ -6,8 +6,12 @@ var gamePattern = [];
 
 function nextSequence(){
   var randomNumber = Math.floor( Math.random() * 4 ) + 1;
+  var randomChosenColor = buttonColors[randomNumber];   //choosing random color using randomNumber
 
-  var randomChooseColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColour); //storing game patterns
 
-  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColor").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100); //flasing the button which is pressed to clicked.
+
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();         // playing sound related button colors.
 }
