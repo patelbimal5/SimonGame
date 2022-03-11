b9df2645c2168ab6495fba9fc71c1bacecfb8989
patelbimal5 <68713222+patelbimal5@users.noreply.github.com>
2022-03-11 14:14:12 -0500

    creating working function

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-alert("Working");
+// alert("Working");
+
+var buttonColors = {"red", "blue", "yellow"};
+
+var gamePattern = [];
+
+function nextSequence(){
+  var randomNumber = Math.floor( Math.random() * 4 ) + 1;
+
+  var randomChooseColor = buttonColors[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+}
